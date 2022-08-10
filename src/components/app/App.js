9de7c52326b2ed757722c16ebd4c1e260293fdc1
petk1234 <petkun8034@gmail.com>
2022-08-10 21:11:52 +0300

    some directory changes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import authOperations from "./redux/auth/authOperations";
-import ListCards from "./components/listCards/ListCards";
-import Form from "./components/form/Form";
-import Header from "./components/header/Header";
-import InfoSection from "./components/info/InfoSection";
-import "./fonts/nunito/Nunito-Regular.woff2";
+import authOperations from "../../redux/auth/authOperations";
+import ListCards from "../listCards/ListCards";
+import Form from "../form/Form";
+import Header from "../header/Header";
+import InfoSection from "../info/InfoSection";
+import "../../fonts/nunito/Nunito-Regular.woff2";
 import "./appStyles.module.scss";
 function App() {
   const dispatch = useDispatch();
